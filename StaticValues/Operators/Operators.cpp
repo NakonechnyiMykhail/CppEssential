debe4cb429ccc0890d6bf070028a4547986b010e
Mikhail <mykhail.nakonechnyi@gmail.com>
2020-11-29 04:04:05 -0800

    manipulators and flags to COUT

--- StaticValues/Operators/Operators.cpp
+++ StaticValues/Operators/Operators.cpp
@@ -2,10 +2,14 @@
 //
 
 #include <iostream>
-
+#include <iomanip>
 class Dollars
 {
 public:
+    Dollars()
+    {
+
+    }
     Dollars(double dollars) 
     { 
         m_dollars = dollars;
@@ -29,6 +33,14 @@ public:
     {
         return Dollars(d1.getDollars() - d2.getDollars());
     }
+    friend Dollars operator-(const Dollars& d1, int value)
+    {
+        return Dollars(d1.getDollars() - value);
+    }
+    friend Dollars operator-(int value, const Dollars& d1)
+    {
+        return Dollars(value - d1.getDollars());
+    }
     void print()
     {
         std::cout << m_dollars << " dollars";
@@ -36,15 +48,35 @@ public:
 
     friend std::ostream& operator<< (std::ostream& out, const Dollars &dol)
     {
-        out << "Dollars: " << dol.m_dollars << '.' << dol.m_cents << std::endl;
+        //out << std::fixed;
+        //out << "Dollars: (" << std::setprecision(2) << dol.m_dollars << ")" << dol.m_dolls << '.' << dol.m_cents << std::endl;
+        out << "Dollars: " << dol.m_dolls << '.' << dol.m_cents << std::endl;
         return out;
+    }
+    friend std::istream& operator>> (std::istream& in, Dollars& dol)
+    { 
+        // in: 7.76 7 76                         => XXXXXXXX 
+        // in: 7.76 visa 4444-4444-44444--4444   => OK
+        in >> dol.m_dollars;
+        //in >> dol.m_dolls;
+        //in >> dol.m_cents;
+        dol.m_dolls = static_cast<int>(dol.m_dollars);
+        // static_cast<DATA_TYPE>(value)
+        // reinterpret_cast<DATA_TYPE>(value)
+        // (DATA_TYPE) value                        C-Style
+        dol.m_cents = static_cast<int>((dol.m_dollars - dol.m_dolls) * 100);
+        // person
+        // student
+        //static_cast<student>(person)
+
+        return in;
     }
 private:
     double m_dollars;
     // 7.76 -> m_dolls => 7 / m_cents = 76
 
-    int m_dolls;
-    int m_cents;
+    int m_dolls; // целая
+    int m_cents; // дробная
 };
 
 
@@ -59,4 +91,11 @@ int main()
 
     Dollars all_dollars = 7 + Dollars(5);
     std::cout << all_dollars;
+    
+    Dollars in_dol;
+    std::cout << "Enter a few dollars: ";
+    std::cin >> in_dol;
+    std::cout << in_dol;
+
+
 }
