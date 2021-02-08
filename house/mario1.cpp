031557e939365a6055e7262bf5abcd4416753f79
mike <mykhail.nakonechnyi@gmail.com>
2021-02-08 10:29:55 +0200

    templates

---
+++ house/mario1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+/*
+     /\ 
+    /  \ 
+   /    \ 
+  /      \ 
+ /--------\
+|          |  
+|          |
+|          |
+|          |
+|__________|
+*/
+
+int main()
+{
+    int height = 10; // INPUT DATA
+    const int roofHeight = height / 2; 
+    
+}
